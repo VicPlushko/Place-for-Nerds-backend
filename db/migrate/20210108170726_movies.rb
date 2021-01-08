6293c7e66233f66401c51222fe6c170162684057
github username <github email address>
2021-01-08 12:23:51 -0500

    restructures database tables

--- db/migrate/20210108170726_movies.rb
+++ db/migrate/20210108170726_movies.rb
@@ -1,15 +1,12 @@
-class CreateMovies < ActiveRecord::Migration[6.0]
+class Movies < ActiveRecord::Migration[6.0]
   def change
     create_table :movies do |t|
       t.string :title
-      t.date :release_date
+      t.string :release_date
       t.text :synopsis
       t.string :poster
-      t.string :actor
+      t.integer :actor_id
       t.string :genre
-      t.string :crew
-
-      t.timestamps
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_05_160225) do
+ActiveRecord::Schema.define(version: 2021_01_08_171416) do
+
+  create_table "actors", force: :cascade do |t|
+    t.string "name"
+    t.integer "cast_id"
+    t.string "profile_path"
+    t.string "character"
+    t.integer "movie_id"
+  end
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
-    t.date "release_date"
+    t.string "release_date"
     t.text "synopsis"
     t.string "poster"
-    t.string "actor"
+    t.integer "actor_id"
     t.string "genre"
-    t.string "crew"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
