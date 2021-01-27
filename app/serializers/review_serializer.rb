ba29bf5d3b0f8824519e2e445b3102ec565fba98
github username <github email address>
2021-01-27 03:27:47 -0500

    makes reviews table controller and model

---
+++ app/serializers/review_serializer.rb
@@ -0,0 +1,4 @@
+class ReviewSerializer < ActiveModel::Serializer
+  attributes :id, :content, :movie_id
+  belongs_to: movie
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :reviews
   get "/movies/search/:searchterm", to: 'movies#search'
   resources :movies
   
