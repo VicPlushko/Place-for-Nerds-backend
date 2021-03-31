afe1245af1e99a68c899038270d39537750087c2
Victor Plushko <vicplushko33@gmail.com>
2021-03-31 14:25:18 -0400

    changes user.create so users will be created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
     skip_before_action :require_login, only: [:create]
     
     def create
-        @user = User.create(user_params)
+        @user = User.create(username: params[:username], email: params[:email], password: params[:password], review_id: params[:review_id])
         if @user.valid?
             payload = {user_id: @user.id}
             token = encode_token(payload)
