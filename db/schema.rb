db64201bb0d245c7c4edb578bb7c7a02adbd4703
Victor Plushko <vicplushko33@gmail.com>
2021-07-28 11:33:55 -0400

    dropped and recreated database in preparation for deployment

--- db/schema.rb
+++ db/schema.rb
@@ -41,4 +41,5 @@ ActiveRecord::Schema.define(version: 2021_04_21_034418) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
+
 end
