482c067efb7cf934532c2ac29e491a3d415083cc
Victor Plushko <vicplushko33@gmail.com>
2021-07-28 13:46:05 -0400

    creates Profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,14 @@
 Rails.application.routes.draw do
+
+  
   resources :tv_show_reviews
   resources :reviews
-  # resources :users, only: [:create]
+  
   post "/register", to: "users#create"
   post "/login", to: "user_auth#login"
   get "/auto_login", to: "user_auth#auto_login"
   get "/is_authed", to: "user_auth#is_authed"
 
-
-  # get "/movies/search/:searchterm", to: 'movies#search'
-  # resources :movies
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
